72d5015ab4b2903669a652df9dd30605d417c780
rico305 <patryk.kozuch.1@gmail.com>
2022-11-18 18:45:48 +0100

    Add docs to Card

--- poker-common/src/main/java/pl/pkozuch/poker/common/Card.java
+++ poker-common/src/main/java/pl/pkozuch/poker/common/Card.java
@@ -2,15 +2,38 @@ package pl.pkozuch.poker.common;
 
 import java.util.Objects;
 
+/**
+ * Representation of Single Card
+ */
 public class Card {
     CardValues value;
     CardSuits suit;
 
-    Card(CardValues value, CardSuits Suit) {
-        this.suit = Suit;
+    /**
+     * Constructs card by passing value and suit
+     *
+     * @param suit  - suit of the card
+     * @param value - value of the card
+     */
+    Card(CardSuits suit, CardValues value) {
+        this.suit = suit;
         this.value = value;
     }
 
+    /**
+     * <p>Constructs card from 2-characters long slug.</p>
+     * <p>First character stands for suit, second stands for value</p>
+     * <p>Possible suits:<br>
+     * 'H' (Heart), 'D' (Diamond), 'S' (Spade), 'C' (Club)
+     * </p>
+     * <p>Possible values:<br>
+     * '2', '3', '4', '5', '6', '7', '8', '9', 'T' (Ten), 'J', 'Q', 'K', 'A' (Ace)
+     * </p>
+     *
+     * <p>Example valid slugs: H2, CT, DA</p>
+     *
+     * @param slug 2 characters long slug
+     */
     Card(String slug) {
         if (slug.length() != 2)
             throw new RuntimeException("Card can be created only from 2-characters long slug. Slug entered: " + slug);
@@ -22,10 +45,20 @@ public class Card {
         this.value = CardValues.getValueByString(valueString);
     }
 
+    /**
+     * Gets card suit
+     *
+     * @return suit of the card
+     */
     public CardSuits getSuit() {
         return suit;
     }
 
+    /**
+     * Gets card value
+     *
+     * @return value of the card
+     */
     public CardValues getValue() {
         return value;
     }
@@ -35,6 +68,13 @@ public class Card {
         return value.getName() + " " + suit.getName();
     }
 
+    /**
+     * Checks if card is <b>RIGHT</b> before other in seniority order
+     * For example, King is <b>RIGHT</b> before Ace in seniority order, so method will return true
+     *
+     * @param other Card to compare
+     * @return True only if card is <b>RIGHT</b> before other in seniority order
+     */
     public boolean isBeforeInSeniorityOrder(Card other) {
         return this.value.getOrder() + 1 == other.value.getOrder();
     }
